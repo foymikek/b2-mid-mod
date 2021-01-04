9cd2fbebf82daffa85d33c7f78f937d6ad63d736
Miquel <foymikek@gmail.com>
2021-01-04 11:50:02 -0700

    Add two working actor model tests

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,13 @@
 class Actor < ApplicationRecord
   has_many :movie_actors
   has_many :movies, through: :movie_actors
+
+  def order_by_age
+    self.order(:age)
+  end
+
+  def average_age
+    self.average(:age)
+  end
+
 end
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -17,9 +17,21 @@ RSpec.describe Actor, type: :model do
       movie_actor_2 = MovieActor.create!(actor_id: actor_2.id, movie_id: movie_1.id)
       movie_actor_3 = MovieActor.create!(actor_id: actor_3.id, movie_id: movie_1.id)
 
-      movie_1_actors = movie_1.actors
-      expet(movie_1_actors.actors_by_age).to eq([actor_2, actor_1, actor_3])
+
+      expect(movie_1.actors.order_by_age).to eq([actor_2, actor_1, actor_3])
+    end
+
+    it "can calculate the average age of the actors" do
+      studio_1 = Studio.create!(name: "Star Productions", location: "Milkyway")
+      movie_1 = studio_1.movies.create!(title: "Louies Adventure", creation_year: "2008", genre: "Comedy")
+      actor_1 = Actor.create!(name: "Denver Dean", age: 22)
+      actor_2 = Actor.create!(name: "Maryloue", age: 20)
+      actor_3 = Actor.create!(name: "Sal", age: 24)
+      movie_actor_1 = MovieActor.create!(actor_id: actor_1.id, movie_id: movie_1.id)
+      movie_actor_2 = MovieActor.create!(actor_id: actor_2.id, movie_id: movie_1.id)
+      movie_actor_3 = MovieActor.create!(actor_id: actor_3.id, movie_id: movie_1.id)
+
+      expect(movie_1.actors.average_age). to eq(22)
     end
   end
-
 end
